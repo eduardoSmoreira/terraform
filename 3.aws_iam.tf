0b04b329251ca268878ed77a23dd6a53e7f9acef
Eduardo Moreira <eduardo.moreira@gmail.com>
2025-02-12 21:07:32 -0300

    aula 17

--- 3.aws_iam.tf
+++ 3.aws_iam.tf
@@ -11,7 +11,7 @@ resource "aws_iam_role" "role_acesso_ssm" {
     assume_role_policy    = "{\"Statement\":[{\"Action\":\"sts:AssumeRole\",\"Effect\":\"Allow\",\"Principal\":{\"Service\":\"ec2.amazonaws.com\"}}],\"Version\":\"2012-10-17\"}"
     description           = "Allows EC2 instances to call AWS services like CloudWatch and Systems Manager on your behalf."
     force_detach_policies = false
-    managed_policy_arns   = ["arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess", "arn:aws:iam::aws:policy/AmazonECS_FullAccess", "arn:aws:iam::aws:policy/AmazonS3FullAccess", "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"]
+    #managed_policy_arns   = ["arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess", "arn:aws:iam::aws:policy/AmazonECS_FullAccess", "arn:aws:iam::aws:policy/AmazonS3FullAccess", "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"]
     max_session_duration  = 3600
     name                  = "role-acesso-ssm"
     name_prefix           = null
@@ -19,24 +19,26 @@ resource "aws_iam_role" "role_acesso_ssm" {
     permissions_boundary  = null
     tags                  = {}
     tags_all              = {}
+}
 
-    /* inline_policy {
-        name   = "policy-get-secret-rds"
-        policy = jsonencode(
-            {
-                Statement = [
-                    {
-                        Action   = [
-                            "secretsmanager:GetSecretValue",
-                        ]
-                        Effect   = "Allow"
-                        Resource = [
-                            "arn:aws:secretsmanager:us-east-1:961341523711:secret:rds!db-34395daa-9fab-446a-ad2e-3523fb432e10-HQN9v9",
-                        ]
-                    },
-                ]
-                Version   = "2012-10-17"
-            }
-        )
-    } */
+resource "aws_iam_role_policy_attachment" "role_acesso_ssm_policy_ecr" {
+  role       = aws_iam_role.role_acesso_ssm.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"
+}
+resource "aws_iam_role_policy_attachment" "role_acesso_ssm_policy_ecs" {
+  role       = aws_iam_role.role_acesso_ssm.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonECS_FullAccess"
+}
+resource "aws_iam_role_policy_attachment" "role_acesso_ssm_policy_s3" {
+  role       = aws_iam_role.role_acesso_ssm.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+resource "aws_iam_role_policy_attachment" "role_acesso_ssm_policy_ssm" {
+  role       = aws_iam_role.role_acesso_ssm.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_role_policy_attachment" "role_acesso_ssm_policy" {
+  role       = aws_iam_role.role_acesso_ssm.name
+  policy_arn = aws_iam_policy.get_secret_bia_db.arn
 }
--- 98.outputs.tf
+++ 98.outputs.tf
@@ -8,12 +8,6 @@ output "instance_type" {
   value       = aws_instance.bia-dev.instance_type
 }
 
-output "instance_security_groups" {
-  description = "SG da EC2"
-  value       = aws_instance.bia-dev.security_groups
-
-}
-
 output "instance_public_id" {
   description = "IP publico da EC2"
   value       = aws_instance.bia-dev.public_ip
